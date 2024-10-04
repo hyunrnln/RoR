0d314ec839d0b1a1493e2bb66254e15321796759
hyunrnln <hyunrnln@.gmail.com>
2024-10-04 05:53:54 +0000

    댓글 게시판

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+	resources :articles do
+		resources :comments, only: [:create]   #댓글이 게시글에 종속되도록 설정
+	end
+	
+  resources :comments
+  resources :articles
   devise_for :users
   resources :tests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-	root 'tests#index'
+	#root 'tests#index'
 
+	root 'articles#index'
 end
